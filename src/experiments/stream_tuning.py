af6233c6024cb25aa106d7da76b7a741ce42f5c4
Tom <veniat.tom@gmail.com>
2021-04-23 19:39:41 +0200

    Test run of s_plus

--- src/experiments/stream_tuning.py
+++ src/experiments/stream_tuning.py
@@ -14,7 +14,6 @@ import pandas
 import ray
 import torch
 import visdom
-import wandb
 from ray import tune
 from ray.tune import CLIReporter
 from ray.tune.logger import JsonLogger, CSVLogger
@@ -39,7 +38,6 @@ logger = logging.getLogger(__name__)
 
 class StreamTuningExperiment(BaseExperiment):
     def run(self):
-        wandb.init(project='CTrL', name=str(self.sacred_run._id))
         if self.task_gen.concept_pool.attribute_similarities is not None:
             attr_sim = self.task_gen.concept_pool.attribute_similarities
             self.main_viz.heatmap(attr_sim,
